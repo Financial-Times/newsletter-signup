f860aac272dd3c3d47fd500f66bfcfebcf1183e7
TheoLeanse <theoleanse@gmail.com>
2016-04-08 09:36:18 +0100

    add interface to anon-email-svc

--- apis/anon-email-svc.js
+++ apis/anon-email-svc.js
@@ -1,18 +1,18 @@
-const apiKey = process.env.ANON_EMAIL_SVC_API_KEY; // TODO: add this to config vars
+const apiKey = process.env.ANON_EMAIL_SVC_API_KEY;
 
 function send(email) {
-    const url = 'https://anon-email-svc-gw-eu-west-1-test.memb.ft.com/send';
+	const url = 'https://anon-email-svc-gw-eu-west-1-test.memb.ft.com/send';
 
-    fetch(url, {
-        headers: {
-            'Content-type': 'application/json',
-            'x-api-key': apiKey
-        },
-        body: JSON.stringify({email: email})
-    });
+	fetch(url, {
+		headers: {
+			'Content-type': 'application/json',
+			'x-api-key': apiKey
+		},
+		body: JSON.stringify({email: email})
+	});
 
 }
 
 export default {
-    send: send
+	send: send
 };
