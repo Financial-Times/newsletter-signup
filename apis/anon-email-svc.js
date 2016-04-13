31af18bb59cc77365412a4481bc5e92027011718
TheoLeanse <theoleanse@gmail.com>
2016-04-13 17:00:54 +0100

    export send function from email svc wrapper

--- apis/anon-email-svc.js
+++ apis/anon-email-svc.js
@@ -1,19 +1,20 @@
 const apiKey = process.env.ANON_EMAIL_SVC_API_KEY;
 
-function send(email) {
-	const url = 'https://anon-email-svc-gw-eu-west-1-prod.memb.ft.com/send';
-
-	fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-type': 'application/json',
-			'x-api-key': apiKey
-		},
-		body: JSON.stringify({email: email})
-	});
-
-}
-
 export default {
-	send: send
+
+	send(email) {
+
+		const url = 'https://anon-email-svc-gw-eu-west-1-prod.memb.ft.com/send';
+
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				'x-api-key': apiKey
+			},
+			body: JSON.stringify({email: email})
+		});
+
+	}
+
 };
