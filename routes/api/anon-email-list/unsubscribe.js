db3afbba9c40eeaaf80a61ab3abc17e2dc5fdece
TheoLeanse <theoleanse@gmail.com>
2016-04-18 14:33:58 +0100

    single view for unsubscribe pages

--- routes/api/anon-email-list/unsubscribe.js
+++ routes/api/anon-email-list/unsubscribe.js
@@ -5,13 +5,20 @@ export default function (req, res) {
 	AnonEmailList.unsubscribe(req.params.user)
 		.then(response => {
 
-			const opts = {
-				layout: 'wrapper'
-			};
+			if (response.status === 204) res.render('light-signup-unsubscribe', {
+				layout: 'wrapper',
+				success: true
+			});
 
-			if (response.status === 204) res.render('light-signup-unsubscribe-success', opts);
-			if (response.status === 403) res.render('light-signup-already-unsubscribed', opts);
-			res.render('light-signup-unsubscribe-failure', opts);
+			if (response.status === 403) res.render('light-signup-unsubscribe', {
+				layout: 'wrapper',
+				alreadyUnsubscribed: true
+			});
+
+			res.render('light-signup-unsubscribe', {
+				layout: 'wrapper',
+				failure: true
+			});
 
 		});
 
