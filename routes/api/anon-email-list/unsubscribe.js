2b2f1115f7f745b3bd0793bfcc5efdb490db09ee
TheoLeanse <theoleanse@gmail.com>
2016-04-20 12:51:52 +0100

    don't try and render the response twice

--- routes/api/anon-email-list/unsubscribe.js
+++ routes/api/anon-email-list/unsubscribe.js
@@ -5,20 +5,31 @@ export default function (req, res) {
 	AnonEmailList.unsubscribe(req.params.user)
 		.then(response => {
 
-			if (response.status === 204) res.render('light-signup-unsubscribe', {
-				layout: 'wrapper',
-				success: true
-			});
+			if (response.status === 204) {
 
-			if (response.status === 403) res.render('light-signup-unsubscribe', {
-				layout: 'wrapper',
-				alreadyUnsubscribed: true
-			});
+				res.render('light-signup-unsubscribe', {
+					layout: 'wrapper',
+					success: true
+				});
 
-			res.render('light-signup-unsubscribe', {
-				layout: 'wrapper',
-				failure: true
-			});
+			}
+
+			if (response.status === 403) {
+
+				res.render('light-signup-unsubscribe', {
+					layout: 'wrapper',
+					alreadyUnsubscribed: true
+				});
+
+			}
+
+			if (response.status !== 204 && response.status !== 403) {
+
+				res.render('light-signup-unsubscribe', {
+					layout: 'wrapper',
+					failure: true
+				});
+			}
 
 		});
 
