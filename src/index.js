71986e9c91d351e6b32ae657637fa59bfd7c6d6c
Matt Brennan <mattyb1000@gmail.com>
2016-05-19 17:21:17 +0100

    explicitly export

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import unsubscribe from './routes/unsubscribe';
 import express from 'express';
 import {urlencoded} from 'body-parser';
 import path from 'path';
+import * as anonEmailLists from './api/anon-email-lists';
+import * as anonEmailSvc from './api/anon-email-svc';
 
 const app = express();
 
@@ -10,3 +12,4 @@ app.post('/', urlencoded({extended: false}), post);
 app.get('/unsubscribe/:user', unsubscribe);
 
 export default app;
+export {anonEmailLists, anonEmailSvc};
