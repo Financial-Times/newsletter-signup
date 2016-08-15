7b3e286bfddbe1e87a4fe41f74459e5b89bf22d7
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-08-15 17:47:09 +0100

    Tidying

--- src/libs/subscribe.js
+++ src/libs/subscribe.js
@@ -1,10 +1,10 @@
+import logger from '@financial-times/n-logger';
+import SpoorClient from '@financial-times/spoor-client';
+
 import { subscribe } from '../api/anon-email-lists';
 import { send } from '../api/anon-email-svc';
-import SpoorClient from '@financial-times/spoor-client';
-import logger from '@financial-times/n-logger';
 
 export default req => {
-
 	const product = req.body && (req.body.product || req.body.source) || null;
 	const mailingList = req.body && req.body.mailingList ? req.body.mailingList : 'light-signup';
 	const topics = req.body && req.body.topics ? req.body.topics : 'default';
@@ -15,8 +15,61 @@ export default req => {
 	const ip = req.get('fastly-client-ip') || req.ip;
 	const deviceId = req.body && req.body.deviceId ? req.body.deviceId : extractDeviceId(cookies);
 
+	const validateRequest = () =>
+		new Promise((resolve, reject) => {
+			if (req.body && req.body.email && (req.body.product || req.body.source)) {
+				if (validateEmailAddress(req.body.email)) {
+					resolve({ });
+				} else {
+					reject({ reason: 'INVALID_EMAIL' });
+				}
+			} else {
+				reject({ reason: 'INVALID_REQUEST' });
+			}
+		});
+
+	const validateEmailAddress = email => /(.+)@(.+)/.test(email);
+
+	const silentlySubmitTrackingEvent = () => {
+		const context = {
+			list: mailingList,
+			content: {
+				uuid: articleUuid,
+			}
+		};
+		if (following) {
+			context.following = following;
+		} else {
+			context.topics = topics;
+		}
+		spoor.submit({ action: 'subscribed', context });
+	};
+
+	const extractDeviceId = cookie => {
+		const id = /spoor-id=([^;]+)/.exec(cookie);
+		return id ? id[1] : null;
+	};
+
+	const subscribeToMailingList = () =>
+		subscribe({
+				email: req.body.email,
+				mailingList,
+				topics,
+				following,
+				deviceId,
+			})
+			.catch(Promise.reject);
+
+	const sendEmailAfter5am = () => {
+		const hourNow = new Date().getHours();
+		if (hourNow >= 5) {
+			send(req.body.email)
+		};
+		return Promise.resolve();
+	};
+
 	if (process.env.LOG_HEADERS) {
-		logger.info(`POST from ${deviceId} with IP ${ip}, headers: ${JSON.stringify(req.headers)}`);
+		logger.info('Subscribing...', Object.assign({}, req.headers, { deviceId, ip}));
 	}
 
 	const spoor = new SpoorClient({
@@ -33,61 +86,4 @@ export default req => {
 		.then(subscribeToMailingList)
 		.then(silentlySubmitTrackingEvent)
 		.then(sendEmailAfter5am);
-
-	function validateRequest () {
-		return new Promise(function (resolve, reject) {
-			if (req.body && req.body.email && (req.body.product || req.body.source)) {
-				if (validateEmailAddress(req.body.email)) {
-					resolve({});
-				} else {
-					reject({ reason: 'INVALID_EMAIL' });
-				}
-			} else {
-				reject({reason: 'INVALID_REQUEST'});
-			}
-		});
-	}
-
-	function validateEmailAddress (email) {
-		return /(.+)@(.+)/.test(email);
-	}
-
-	function silentlySubmitTrackingEvent () {
-		const context = {
-			list: mailingList,
-			content: {
-				uuid: articleUuid,
-			}
-		};
-		if (following) {
-			context.following = following;
-		} else {
-			context.topics = topics;
-		}
-		spoor.submit({ action: 'subscribed', context });
-	}
-
-	function extractDeviceId (cookie) {
-		const id = /spoor-id=([^;]+)/.exec(cookie);
-		return (id) ? id[1] : null;
-	}
-
-	function subscribeToMailingList () {
-		return subscribe({
-			email: req.body.email,
-			mailingList,
-			topics,
-			following,
-			deviceId,
-		})
-		.catch(error => {
-			return Promise.reject(error);
-		});
-	}
-
-	function sendEmailAfter5am () {
-		const hourNow = new Date().getHours();
-		if (hourNow >= 5) send(req.body.email);
-		return Promise.resolve();
-	}
-}
+};
